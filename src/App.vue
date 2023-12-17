1340d7cc5fd9111a1adae24db798d628f828dae2
sairlseb <cyrilseberiaga@gmail.com>
2023-12-17 16:51:15 +0800

    added vue router and initial homepage

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,37 @@
   <v-app>
     <v-app-bar title="Sai Portfolio" elevation="2">
       <v-spacer></v-spacer>
-      <v-btn text>Home</v-btn>
+      <v-btn text @click="routeTo('/')">Home</v-btn>
       <v-btn text>About</v-btn>
       <v-btn text>Projects</v-btn>
       <v-btn text>Contact</v-btn>
     </v-app-bar>
-    <homepage></homepage>
+   <v-main class="mt-3">
+    <router-view></router-view>
+   </v-main>
   </v-app>
- 
+
 </template>
 
 <script>
-import Homepage from './components/Homepage.vue'
+import Homepage from './components/views/Homepage.vue'
+import {useRouter} from 'vue-router'
 
 export default {
   name: 'App',
   components: {
     Homepage
+  },
+  setup() {
+     
+    const router = useRouter()
+    const routeTo = (path) => {
+      router.push(path)
+    }
+
+    return {
+      routeTo
+    }
   }
 }
 </script>
