e8edb8154b740901017f2a56c45426b92b1b3157
sairlseb <cyrilseberiaga@gmail.com>
2023-12-31 11:46:47 +0800

    update in deploy file

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sairilseb-me/sai-portfolio.git develop:gh-pages
+git push -f git@github.com:sairilseb-me/sai-portfolio.git main:gh-pages
 
 cd -
