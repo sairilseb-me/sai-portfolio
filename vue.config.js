c7cdee09ecc65ac8fec8e205b9995678695f5f46
sairlseb <cyrilseberiaga@gmail.com>
2024-01-06 13:05:23 +0800

    updated url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/sai-portfolio" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 })
