ea7d41ee425ebcfa8a7eaed287100760eaec1008
sairlseb <cyrilseberiaga@gmail.com>
2025-06-14 12:36:56 +0800

    added a privacy policy page

--- src/components/views/layouts/vertical-nav.vue
+++ src/components/views/layouts/vertical-nav.vue
@@ -56,6 +56,10 @@ export default {
             {
                 title: 'Contact',
                 link: '/contact'
+            },
+            {
+                title: 'Privacy Policy',
+                link: '/privacy-policy'
             }
         ]
 
